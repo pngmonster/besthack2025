627aa5861b17a59a17bb22d16568bbd7634f5b49
giicoo <giicoo.gm@gmail.com>
2025-11-16 08:02:49 +0300

    HEEEELP

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,12 +3,14 @@ import chromadb
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from app.core.config import configs
 from app.model.base_model import Base
+from sentence_transformers import SentenceTransformer
 
-
+MODEL_NAME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 engine = create_async_engine(configs.DATABASE_URI, echo=False, future=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-chroma = chroma_client = chromadb.HttpClient(host='localhost', port=8001)
+chroma = chroma_client = chromadb.Client()
+model = SentenceTransformer(MODEL_NAME)
 
 collection = chroma.create_collection(
     name="addresses",
