e51bad0c0d9dc7fe77379e75fd0ce641609a6013
giicoo <giicoo.gm@gmail.com>
2025-11-16 08:29:49 +0300

    HEEEELP121431

--- backend/app/repository/chromadb_repository.py
+++ backend/app/repository/chromadb_repository.py
@@ -2,6 +2,7 @@ import chromadb
 from typing import List
 from fastapi import Depends
 from app.core.database import get_chroma
+from sentence_transformers import SentenceTransformer
 
 class ChromaRepository:
     def __init__(self, collection: chromadb.Collection):
@@ -11,9 +12,15 @@ class ChromaRepository:
         self.collection.add(ids=ids, documents=texts, embeddings=embedding)
 
     async def query(self, search_address: str, n_results: int):
+        MODEL_NAME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+        model = SentenceTransformer(MODEL_NAME)
+        emb = model.encode(
+               search_address,
+               convert_to_numpy=False,   # возвращает torch.Tensor
+           )
         result = self.collection.query(
-            query_texts=search_address,
-            n_results=n_results
+            query_embeddings=[emb],
+            n_results=n_results,
         )
         ids = result["ids"][0]
         distances = result["distances"][0]
